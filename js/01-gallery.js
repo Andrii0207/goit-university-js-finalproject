ee3ac041d5ec43f9e909bca4e5a71673970785e2
Andrii <Popov.Andrey80@gmail.com>
2023-10-21 22:52:33 +0300

    fix first task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,37 +1,37 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
-
 const listEl = document.querySelector(".gallery")
 
-
 function createGallery(gallery) {
-    return gallery.map(({ preview, original, description }) => `<li class="gallery__item" >
+    return gallery.map(({ preview, original, description }) =>
+        `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
     <img
-            class="gallery__image"
-            src="${preview}"
-            data-source="${original}"
-            alt="${description}"
-        />
-</li >`).join("")
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`).join("")
 }
 
 listEl.insertAdjacentHTML('beforeend', createGallery(galleryItems));
 listEl.addEventListener("click", onOpenModal)
 
 function onOpenModal(e) {
-    const selectedImage = e.target.classList.contains("gallery__image")
-    const originalSizeImg = e.target.dataset.source
-    const altImg = e.target.alt
+    e.preventDefault()
 
-    console.log(originalSizeImg)
+    const selectedImage = e.target.classList.contains("gallery__image");
+    const originalSizeImg = e.target.dataset.source;
+    const altImg = e.target.alt;
 
     if (!selectedImage) {
         return
     }
 
-    const instance = basicLightbox.create(`<img src="${originalSizeImg}" alt="${altImg}"/>`, {
+    const instance = basicLightbox.create(`<div class="modal"><img src="${originalSizeImg}" alt="${altImg}"/></div>`, {
         closable: false,
     })
     instance.show(() => console.log("Lightbox now visible"))
@@ -44,9 +44,3 @@ function onOpenModal(e) {
         }
     }
 }
-
-
-
-// <a class="gallery__link" href="${original}">
-        
-    // </a>
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,11 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector(".gallery")
+
+function createGallery(galleryItems) {
+
+    console.log(galleryItems)
+
+}
