c6914ae1c1ad82f16ff43116607dc80d56126d21
Andrii <Popov.Andrey80@gmail.com>
2023-10-22 00:36:21 +0300

    add second task

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,12 +1,37 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
-
 const galleryEl = document.querySelector(".gallery")
 
 function createGallery(galleryItems) {
 
-    console.log(galleryItems)
-
+    return galleryItems.map(({ preview, original, description }) => `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>`).join("")
 }
+
+galleryEl.insertAdjacentHTML("beforeend", createGallery(galleryItems))
+
+galleryEl.addEventListener("click", onOpenModal)
+
+function onOpenModal(e) {
+    e.preventDefault()
+
+    const isImage = e.target.classList.contains("gallery__image");
+
+    if (!isImage) {
+        return console.log("It's not a image")
+    }
+
+    new SimpleLightbox(".gallery a", {
+        showCounter: false,
+        captionDelay: 250,
+        captionPosition: "bottom",
+        close: false,
+        overlayOpacity: 1,
+        captionsData: `alt`
+    })
+}
+
